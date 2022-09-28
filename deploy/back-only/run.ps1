d388ceb335e1d610174f0a40eeb14e995a6bd1fc
LinHeLurking <LinHe.Lurking@gmail.com>
2022-09-28 11:13:32 +0800

    Fix path bugs;

--- deploy/back-only/run.ps1
+++ deploy/back-only/run.ps1
@@ -1,14 +1,13 @@
 $Self = Split-Path -Parent $MyInvocation.MyCommand.Source
 # Write-Output $Self
 
-$JarName = (Get-ChildItem -Path $Self\app\* -Include "*.jar")[0].FullName
-$JarName = $JarName -replace "\\", "/"
+$JarName = (Get-ChildItem -Path $Self\app\* -Include "*.jar")[0].Name
 # Write-Output $JarName
 
 $ComposeYml = (Get-Content $Self/docker-compose.template).
-replace("__WEB_SRC", "$Self/app"). 
+replace("__WEB_SRC", "$Self\\app"). 
 replace("__JAR_NAME", "$JarName").
-replace( "__DB_INIT", "$Self/db-init")
+replace( "__DB_INIT", "$Self\\db-init")
 # Write-Output $ComposeYml
 $TmpFile = New-TemporaryFile
 $ComposeYml | Out-File -Path $TmpFile.FullName
