fa53c895be98d7f93c32add41c55406d0be586b7
LinHeLurking <LinHe.Lurking@gmail.com>
2022-09-28 11:09:25 +0800

    powershell script support;

---
+++ deploy/back-only/run.ps1
@@ -0,0 +1,16 @@
+$Self = Split-Path -Parent $MyInvocation.MyCommand.Source
+# Write-Output $Self
+
+$JarName = (Get-ChildItem -Path $Self\app\* -Include "*.jar")[0].FullName
+$JarName = $JarName -replace "\\", "/"
+# Write-Output $JarName
+
+$ComposeYml = (Get-Content $Self/docker-compose.template).
+replace("__WEB_SRC", "$Self/app"). 
+replace("__JAR_NAME", "$JarName").
+replace( "__DB_INIT", "$Self/db-init")
+# Write-Output $ComposeYml
+$TmpFile = New-TemporaryFile
+$ComposeYml | Out-File -Path $TmpFile.FullName
+docker.exe compose --file $TmpFile.FullName up
+docker.exe compose --file $TmpFile.FullName rm -fsv
